5e00bccf7b23bc83d76eb3251a8adadcd9e2557a
Abdisemed Abdullahi <abdisemed20@gmail.com>
2023-11-27 13:12:39 +0300

    completed

--- lib/parrot.py
+++ lib/parrot.py
@@ -1,2 +1,12 @@
-def parrot():
-    pass
+def parrot(string=None):
+    if  string is not None and string  == f"{string}":
+        print(f"{string}")
+        return string
+    else:
+        print("Squawk!")
+        return "Squawk!"
+
+
+parrot("Mankulo")
+
+parrot()
